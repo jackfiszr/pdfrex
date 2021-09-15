77982ce9c72948ef18a252f7ad71f9e8d8b7cd89
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-09-15 20:40:55 +0200

    chore(merge): revert to simple version

--- merge.ts
+++ merge.ts
@@ -1,20 +1,28 @@
-import { PDFDocument } from "./deps.ts";
+import { expandGlobSync, join, PDFDocument, PDFPage } from "./deps.ts";
+
+export async function mergeAll() {
+  const globString = join(Deno.cwd(), "*.pdf");
+  const pdfsToMerge = Array.from(expandGlobSync(globString)).map((entry) =>
+    entry.path
+  );
+  await mergePdfs(pdfsToMerge);
+}
 
 export async function mergePdfs(pdfsToMerge: string[]) {
   const mergedPdf = await PDFDocument.create();
   for (const pdfPath of pdfsToMerge) {
     const pdfBytes = Deno.readFileSync(pdfPath);
     const pdfDoc = await PDFDocument.load(pdfBytes);
-    if (pdfDoc.getPageCount() === 2) {
-      const copiedPages = await mergedPdf.copyPages(
-        pdfDoc,
-        pdfDoc.getPageIndices(),
-      );
-      copiedPages.forEach((page: any) => {
-        mergedPdf.addPage(page);
-      });
-    }
+    const copiedPages = await mergedPdf.copyPages(
+      pdfDoc,
+      pdfDoc.getPageIndices(),
+    );
+    copiedPages.forEach((page: PDFPage) => {
+      mergedPdf.addPage(page);
+    });
   }
   const mergedPdfFile = await mergedPdf.save();
-  Deno.writeFileSync("./output/merged.pdf", mergedPdfFile);
+  Deno.writeFileSync("merged_full.pdf", mergedPdfFile);
 }
+
+if (import.meta.main) mergeAll();
